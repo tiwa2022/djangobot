8533bee29385af956058462971e3449920fcdf28
Rich Hildred <rhildred@gmail.com>
2021-03-18 18:36:27 -0400

    let it be formatted

--- sms/models.py
+++ sms/models.py
@@ -2,17 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
 class Order(models.Model):
     phone = models.CharField(max_length=255, default='')
     data = models.JSONField()
+
     def handleInput(self, sInput):
         # self.data["state"] starts out as WELCOMING
         aReturn = []
         return aReturn
+
     def isDone(self):
         if self.data["state"] == "DONE":
             return True
         else:
             return False
+
     class Meta:
-       indexes = [models.Index(fields=['phone'])]
+        indexes = [models.Index(fields=['phone'])]
