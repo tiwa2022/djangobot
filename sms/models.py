6f8d86123007a7e400d982dd7e7a911e79e7fe9a
Rich Hildred <rhildred@gmail.com>
2021-03-18 18:24:46 -0400

    add framework for finishing

--- sms/models.py
+++ sms/models.py
@@ -6,13 +6,11 @@ class Order(models.Model):
     phone = models.CharField(max_length=255, default='')
     data = models.JSONField()
     def handleInput(self, sInput):
+        # self.data["state"] starts out as WELCOMING
         aReturn = []
-        aReturn.append("Message from bot isn't the opinion of the programmer")
-        aReturn.append("I know you are " + sInput + " but what am I?")
-        try:
-            self.data['sInput'] += ", " + sInput
-        except:
-            self.data['sInput'] = sInput
         return aReturn
     def isDone(self):
-        return True
+        if self.data["state"] == "DONE":
+            return True
+        else:
+            return False
