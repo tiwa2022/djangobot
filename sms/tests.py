f968e4a1e3bb1cbb9aab10e17fda544f85a73956
Rich Hildred <rhildred@gmail.com>
2021-03-18 21:11:36 -0400

    move test to a TestCase

--- sms/tests.py
+++ sms/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+import sqlite3
 
 # Create your tests here.
+
+
+class SMSTests(TestCase):
+    def test_for_json_support(self):
+        conn = sqlite3.connect(':memory:')
+        cursor = conn.cursor()
+        try:
+            cursor.execute('SELECT JSON(\'{"a": "b"}\')')
+            self.assertIs(False, False)
+        except:
+            self.assertIs(True, False)
--- test.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect(':memory:')
-cursor = conn.cursor()
-try:
-    cursor.execute('SELECT JSON(\'{"a": "b"}\')')
-    print("the JSON capability is there")
-except:
-    print("the JSON capability is not there")
