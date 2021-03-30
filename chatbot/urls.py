8a1bfd0ebd95101b7ba035e5503c573253125e3b
Rich Hildred <rhildred@gmail.com>
2021-03-30 14:13:51 -0400

    order display

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,6 +5,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^simulator/', TemplateView.as_view(template_name="index.html")),
     url(r'^sms/', include('sms.urls')),
+    url('', include('orders.urls')),
 ]
